5ed16a3d0b6b4e97750d7e8b97e5e7a39db10823
Graham Hart <graham@the-harts.co.uk>
2021-03-07 11:11:05 +0000

    Version 1.02 - Supports FTDNA files. Same as 23andMe but are comma separated. UNtested because I don't have a file for them. - Fix bug with Green segments disappearing if you changed the colour of the SNPs - Default colours to forestgreen and limegreen

--- pixel_config.py
+++ pixel_config.py
@@ -6,7 +6,7 @@ There are TWO MANDATORY settings:
 
 DATA_FILE_DIRECTORY = 'raw_files'
 IMAGE_FILE_DIRECTORY = 'images'
-siblings_to_render = ['KEN', 'ALAN', 'BRIAN','ADRIAN']
+siblings_to_render = ['KEN', 'ALAN', 'BRIAN']
 
 # optionally you can compare the siblings with one additional relative
 extra_match = 'BERYL'
@@ -30,36 +30,42 @@ copy all relevant raw dna files into the directory your specify
 """
 SINGLE_CHROMOSOME = True
 
+
+# Settings for recombination lines
 INCLUDE_RECOMBINATION_LINES=True
 RECOMBINATION_LINE_WIDTH = 20
 RECOMBINATION_LINE_COLOR = 'black'
 
-# Filter out any entries with 'I', 'D' or '0' ()
+# Filter out any entries with 'I', 'D' or '0' and whether to 
+# only include SNPs which are in all the kits
 FILTER_INSERTS = True
 FILTER_DELETES = True
 FILTER_NO_CALLS = True
 FILTER_MISSING_SNPS = True
 
+# Remove any SNP where every kit has the same value for all alleles
 # removes a lot of 'noise' SNPs that don't contribute to the analysis
 FILTER_COMPLETELY_MATCHED_SEGMENTS = True
 COLOUR_COMPLETELY_MATCHED_SEGMENTS = False
 
-# optional settings to change appearance of rendered chromosome pairs  #
-FULLY_IDENTICAL_SNP_COLOR = 'limegreen'
+# Set the colours of the lines in the output
+# For the top lines
+FULLY_IDENTICAL_SNP_COLOR = 'forestgreen'
 NO_MATCH_SNP_COLOR = 'red'
 HALF_IDENTICAL_SNP_COLOR = 'yellow'
-
+# For the matching lines
 FULL_MATCH_SEGMENT_COLOR = 'limegreen'
 HALF_MATCH_SEGMENT_COLOR = 'yellow'
 NO_MATCH_SEGMENT_COLOR = 'red'
 
-
+# Set colours for the optional SNPs if they are not filtered out
 DELETE_SNP_COLOR = 'gray'
 INSERT_SNP_COLOR = 'gray'
 NO_CALL_SNP_COLOR = 'gray'
 MISSING_SNP_COLOR = 'orange'
 COMPLETELY_MATCHED_SNP_COLOR = 'gray'
 
+# Base colours for the graphics
 BACKGROUND_COLOR = 'lightsteelblue'
 CHROMOSOME_BASE_COLOR = 'white'
 
@@ -70,6 +76,7 @@ GREEN_GAP = 1
 MIN_REDS = 5
 MIN_GREENS = 100
 
+# Control the size of the image and the text 
 WIDTH_OF_SNP_LINE = 1
 HEIGHT_OF_CHROMOSOME_IMAGE = 600
 SPACE_BETWEEN_MATCHES = 300
